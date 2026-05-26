f831250f25d34f2b1600f6118d21051e0bd0a0ef
Alexandre Brunel <lenurbinho@gmail.com>
2026-05-26 21:38:22 +0200

    Update recherche_minima_v5.py

--- recherche_minima_v5.py
+++ recherche_minima_v5.py
@@ -31,7 +31,7 @@ def resource_path(relative_path):
 # ==============================================================================
 # Format : (Date Début, Date Fin)
 PERIODES_MINIMA = {
-    "ce":  (datetime(2026, 4, 20), datetime(2026, 7, 26)),
+    "ce":  (datetime(2026, 5, 1), datetime(2026, 7, 26)),
     "u20": (datetime(2026, 4, 1), datetime(2026, 7, 19)),
     "u18": (datetime(2026, 4, 1), datetime(2026, 7, 5))
 }
